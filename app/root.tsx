a40a097908fee9fc632eb4218f75c76f978cf871
seikode <64799923+seikode@users.noreply.github.com>
2025-05-14 15:50:13 +0900

    🎉 begin

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
 import {
   Form,
+  Link,
+  Outlet,
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
@@ -7,12 +9,16 @@ import {
 import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
-export default function App() {
+export default function App({ loaderData }: Route.ComponentProps) {
+  const { contacts } = loaderData;
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1>
+          <Link to="about">React Router Contacts</Link>
+        </h1>
         <div>
           <Form id="search-form" role="search">
             <input
@@ -29,15 +35,32 @@ export default function App() {
           </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? <span>★</span> : null}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
       </div>
     </>
   );
@@ -91,5 +114,19 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
     </main>
+  );
+}
+
+export async function clientLoader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
